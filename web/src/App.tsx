6ff28e9249fad376d1599d3a65c306a644af97f4
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-02-08 23:20:54 -0300

    starting web project

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {Habit} from "./components/Habit";
+
+function App() {
+  return (
+      <div>
+          <h1>Hello world</h1>
+
+          <Habit />
+          <Habit />
+          <Habit />
+          <Habit />
+          <Habit />
+      </div>
+  )
+}
+
+export default App
